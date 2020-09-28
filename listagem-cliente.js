cbafbabb5e353fb320f67cf293e4cf0004efed24
Henrique Pinho <henrique.de.nf@gmail.com>
2020-09-28 16:49:03 -0300

    Aula 2

--- listagem-cliente.js
+++ listagem-cliente.js
@@ -1,19 +1,12 @@
-const infoClientes = [
-  {
-    cpf: 12345678912,
-    nome: 'Arnaldo'
-  },
-  {
-    cpf: 12345678913,
-    nome: 'Arnalda'
-  }
-];
+
 
 const corpoTabela = document.querySelector("[data-conteudo]");
 
-infoClientes.forEach(indice => {
-  corpoTabela.appendChild(exibeCliente(indice.cpf, indice.nome));
-});
+listarClientes().then( exibe => {
+  exibe.forEach(indice => {
+    corpoTabela.appendChild(exibeCliente(indice.cpf, indice.nome));
+  });
+})
 
 function exibeCliente(cpf, nome) {
   const linha = document.createElement('tr');
